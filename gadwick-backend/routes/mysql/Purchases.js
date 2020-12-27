bd451727cfe7f6e7d62b77d2705de0360fff62b9
Xaoka <powellrowan@yahoo.co.uk>
2020-12-27 14:59:18 +0000

    Fixed cors

--- gadwick-backend/routes/mysql/Purchases.js
+++ gadwick-backend/routes/mysql/Purchases.js
@@ -29,40 +29,40 @@ router.get('/:user_id', cors(corsOptions), async function(req, res, next) {
     res.send(response);
 });
 
-app.post("/create-checkout-session", async (req, res) => {
-    const { priceId } = req.body;
+// router.post("/create-checkout-session", async (req, res) => {
+//     const { priceId } = req.body;
   
-    // See https://stripe.com/docs/api/checkout/sessions/create
-    // for additional parameters to pass.
-    try {
-      const session = await stripe.checkout.sessions.create({
-        mode: "subscription",
-        payment_method_types: ["card"],
-        line_items: [
-          {
-            price: priceId,
-            // For metered billing, do not pass quantity
-            quantity: 1,
-          },
-        ],
-        // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
-        // the actual Session ID is returned in the query parameter when your customer
-        // is redirected to the success page.
-        success_url: 'https://example.com/success.html?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url: 'https://example.com/canceled.html',
-      });
+//     // See https://stripe.com/docs/api/checkout/sessions/create
+//     // for additional parameters to pass.
+//     try {
+//       const session = await stripe.checkout.sessions.create({
+//         mode: "subscription",
+//         payment_method_types: ["card"],
+//         line_items: [
+//           {
+//             price: priceId,
+//             // For metered billing, do not pass quantity
+//             quantity: 1,
+//           },
+//         ],
+//         // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+//         // the actual Session ID is returned in the query parameter when your customer
+//         // is redirected to the success page.
+//         success_url: 'https://example.com/success.html?session_id={CHECKOUT_SESSION_ID}',
+//         cancel_url: 'https://example.com/canceled.html',
+//       });
   
-      res.send({
-        sessionId: session.id,
-      });
-    } catch (e) {
-      res.status(400);
-      return res.send({
-        error: {
-          message: e.message,
-        }
-      });
-    }
-  });
+//       res.send({
+//         sessionId: session.id,
+//       });
+//     } catch (e) {
+//       res.status(400);
+//       return res.send({
+//         error: {
+//           message: e.message,
+//         }
+//       });
+//     }
+//   });
   
 module.exports = router;
